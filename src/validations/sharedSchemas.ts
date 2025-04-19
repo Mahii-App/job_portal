60f8d3e4e1e7fb16203bda6b75676f047664d475
Mahi-App <mahi.rajput@appinventiv.com>
2025-04-19 23:40:23 +0530

    initial commit

--- src/validations/sharedSchemas.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const sharedSchemas = {
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-  objectId: Joi.string().hex().length(24).required(),
-  name: Joi.string().required(),
-};
--- src/validations/superAdminValidations.ts
+++
@@ -1,6 +0,0 @@
-import Joi from 'joi';
-import { sharedSchemas } from './sharedSchemas';
-
-export const deleteUserSchema = Joi.object({
-  userId: sharedSchemas.objectId,
-});
